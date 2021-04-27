9697e7946955cedb5a0607692cb8e233a5162f44
flashbtw <johannes-kurt.reiter@freenet.de>
2021-04-27 10:40:10 +0200

    initial commit

---
+++ src/main/java/de/flash/spigot/hardcoremultiplayer/HardcoreMultiplayer.java
@@ -0,0 +1,17 @@
+package de.flash.spigot.hardcoremultiplayer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcoreMultiplayer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
